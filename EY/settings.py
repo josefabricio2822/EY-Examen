8de047f04457e46dd77f722bce365bbe9045dca3
josefabricio2822 <jf.chamanl@alum.up.edu.pe>
2022-02-11 08:51:33 -0500

    heroku ready commit

--- EY/settings.py
+++ EY/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_URL = '/static/'
+STATIC_ROOT = '/inventario/static/'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'articulo-list'
 
